e255676bcedfadabd8fa3e9595f8a35f8f70d9a0
Serkan Muhcu <serkan.muhcu@student.uni-tuebingen.de>
2020-08-14 04:11:08 +0200

    Add post-incrementing load and store operations

--- llvm/lib/Target/RISCV/AsmParser/RISCVAsmParser.cpp
+++ llvm/lib/Target/RISCV/AsmParser/RISCVAsmParser.cpp
@@ -1417,6 +1417,13 @@ RISCVAsmParser::parseMemOpBaseReg(OperandVector &Operands) {
     return MatchOperand_ParseFail;
   }
 
+  if (getSTI().getFeatureBits()[RISCV::FeaturePulp]) {
+    if (getLexer().is(AsmToken::Exclaim)){
+      getParser().Lex(); // Eat '!'
+      Operands.push_back(RISCVOperand::createToken("!", getLoc(), isRV64()));
+    }
+  }
+
   if (getLexer().isNot(AsmToken::RParen)) {
     Error(getLoc(), "expected ')'");
     return MatchOperand_ParseFail;
@@ -1509,8 +1516,13 @@ bool RISCVAsmParser::parseOperand(OperandVector &Operands, StringRef Mnemonic) {
     return true;
 
   // Attempt to parse token as a register.
-  if (parseRegister(Operands, true) == MatchOperand_Success)
+  if (parseRegister(Operands, true) == MatchOperand_Success) {
+    if (getSTI().getFeatureBits()[RISCV::FeaturePulp]) {
+      if (getLexer().is(AsmToken::LParen))
+        return parseMemOpBaseReg(Operands) != MatchOperand_Success;
+    }
     return false;
+  }
 
   // Attempt to parse token as an immediate
   if (parseImmediate(Operands) == MatchOperand_Success) {
